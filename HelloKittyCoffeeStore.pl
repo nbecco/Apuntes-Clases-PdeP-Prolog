0c29be3abfbe72480a4aab905241222cd2c97dbb
nbecco <nbecco@frba.utn.edu.ar>
2026-08-08 19:36:36 -0300

    Se completo el ejercicio HelloKittyCoffeeStore

--- HelloKittyCoffeeStore.pl
+++ HelloKittyCoffeeStore.pl
@@ -2,14 +2,14 @@
     pedido(kuromi, bebida(mocca, frio, chico)).
     pedido(kuromi, pastel(torta, 2, [chocolate, crema])).
     pedido(melody, bebida(pinkLatte, caliente, grande)).
-    pedido(melody, pastel(torta, 4, [frutilla, cereza, vainilla, crema])).
+    pedido(melody, pastel(torta, 1, [frutilla, cereza, vainilla, crema])).
     pedido(cinnamoroll, pastel(cinnamoroll, 1, [canela, crema, vainilla])).
     pedido(pompompurin, bebida(cafeConLeche, caliente, chico)).
     pedido(chococat, pastel(torta, 3, [chocolate])).
     pedido(dearDaniel, bebida(matchaLatteLabubu, caliente, grande)).
-    pedido(dearDaniel, pastel(torta, 3, [durazno, vainilla])).
+    pedido(dearDaniel, pastel(torta, 3, [durazno, vainilla, anana])).
     pedido(badtzMaru, bebida(cafe, caliente, mediano)).
-    keruppi(vasoAgua).
+    pedido(keroppi, agua).
     pedido(cinnamoroll, merchandising(taza, cinnamoroll)).
     pedido(dearDaniel, merchandising(taza, helloKitty)).
 
@@ -20,26 +20,32 @@
     precio(bebida(_, _, mediano), 6000).
     precio(bebida(_, _, grande), 8000).
 
-    precio(pasteleria(_, Capas, Sabores), Precio) :-
+    precio(pastel(_, Capas, Sabores), Precio) :-
         length(Sabores, CantidadSabores),
         Precio is 2000 * Capas + 800 * CantidadSabores.
 
-    precio(merchandising(_, HelloKitty), 8000).
-    precio(merchandising(_, Kuromi), 8000).
+    precio(merchandising(_, helloKitty), 8000).
+    precio(merchandising(_, kuromi), 8000).
     precio(merchandising(_, Personaje), 6000) :-
-        Personaje = helloKitty,
-        Personaje = kuromi.
+        Personaje \= helloKitty,
+        Personaje \= kuromi.
 
 
 % 3)
+compra(Cliente, NumeroDePedidos):-
+    pedido(Cliente, _),
+    findall(Pedido, pedido(Cliente, Pedido), Pedidos),
+    length(Pedidos, NumeroDePedidos).
 
 
 % 4)
-
+noLeGustaCrema(Cliente):-
+    pedido(Cliente, pastel(_, _, ListaSabor)),
+    not(member(crema, ListaSabor)).
 
 % 5)
     realizaPedidoCaro(Cliente):-
-        personaje(Cliente),
+    pedido(Cliente, _), %ESTA BIEN IMPLEMENTAR ESTA LINEA POR la Inversibilidad (es un Generador de Inversibilidad)
         forall(pedido(Cliente, Pedido), esCaro(Pedido)).
 
     esCaro(Pedido) :- 
@@ -48,7 +54,13 @@
 
 
 % 6)
+gastoTotalPorCliente(Cliente, TotalGasto) :-
+    pedido(Cliente, _),
+    % El findall ahora recolecta Precios y vincula el pedido con su precio
+    findall(Precio, (pedido(Cliente, Pedido), precio(Pedido, Precio)), ListaDePrecios),
+    sumlist(ListaDePrecios, TotalGasto).
 
-
-
-% 7)
+clienteQueMasGasto(Cliente) :-
+    gastoTotalPorCliente(Cliente, GastoMaximo),
+    % Verificamos que para cualquier otro gasto de otro cliente, el de nuestro Cliente sea mayor o igual
+    forall(gastoTotalPorCliente(_, OtroGasto), GastoMaximo >= OtroGasto).
